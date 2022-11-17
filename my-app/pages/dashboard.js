409a19ce94e387bd747b105aeb9d1542b9147b67
Debangsu Das <debangsudas90@outlook.com>
2022-11-17 23:42:56 +0530

    dashboard

--- my-app/pages/dashboard.js
+++ my-app/pages/dashboard.js
@@ -2,10 +2,9 @@ import router, { useRouter } from "next/router";
 import Web3Modal from "web3modal";
 import { providers, Contract } from "ethers";
 import { useEffect, useRef, useState } from "react";
-import Card from '../components/Card/Card.jsx'
-import useAuth from '../hooks/useAuth'
-import { getStatic } from 'ethers/lib/utils.js';
-
+import Card from "../components/Card/Card.jsx";
+import useAuth from "../hooks/useAuth";
+import { getStatic } from "ethers/lib/utils.js";
 
 /*
  * This function returns the first linked account found.
@@ -19,13 +18,12 @@ function dashboard() {
   const web3ModalRef = useRef();
   const [currentAccount, setCurrentAccount] = useState("");
   const [cardDetails, setCardDetails] = useState([]);
-  const FormData=JSON.parse(localStorage.getItem('formData'))
-  const Candidates=JSON.parse(localStorage.getItem('people'))
-  console.log( Candidates)
+  const FormData = JSON.parse(localStorage.getItem("formData"));
+  const Candidates = JSON.parse(localStorage.getItem("people"));
+  console.log(Candidates);
 
-  
-
-   const getEthereumObject=()=> window.ethereum || window.web3?.currentProvider
+  const getEthereumObject = () =>
+    window.ethereum || window.web3?.currentProvider;
   const getProviderOrSigner = async (needSigner = false) => {
     // Connect to Metamask
     // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
@@ -73,7 +71,7 @@ function dashboard() {
       console.error(err);
     }
   };
-  
+
   const renderButton = () => {
     if (!walletConnected) {
       return (
@@ -96,17 +94,11 @@ function dashboard() {
         </button>
       );
     }
-     else{
-      return  <button className='bg-[#bd3fb8] mt-[1px] fixed px-6 py-2 rounded-xl
-      text-white font-semibold text-sm top-4 z-50 right-[10rem]'
-     onClick={disconnectWallet} 
-     >Disconnect Wallet</button>
-     }
-     }
-    
-    useEffect(() => {
-      renderButton()
-    }, [])
+  };
+
+  useEffect(() => {
+    renderButton();
+  }, []);
 
   return (
     <div className="flex w-screen m-0  h-screen">
@@ -152,30 +144,35 @@ function dashboard() {
             <h3>avatar</h3>
             <h3>name</h3>
           </div>
-          <div className='flex flex-col mt-20 px-4'>
-            <h1 className='font-bold text-2xl'>Your Vote is Secure, Your Vote Counts</h1>
-            <p className='px-1 text-sm font-normal mt-2 text-gray-500'>znbvjsdbvjkfdkjvbkjfbvkjsdnv kjdvkjnjk</p>
+          <div className="flex flex-col mt-20 px-4">
+            <h1 className="font-bold text-2xl">
+              Your Vote is Secure, Your Vote Counts
+            </h1>
+            <p className="px-1 text-sm font-normal mt-2 text-gray-500">
+              znbvjsdbvjkfdkjvbkjfbvkjsdnv kjdvkjnjk
+            </p>
           </div>
-          <div className='flex mt-5 mx-[11px]'>
-
+          <div className="flex mt-5 mx-[11px]">
             <div className="w-[10px] h-[10px] ml-3 mt-[6.7px] bg-[#93278F] rounded-full"></div>
             <span className="font-semibold px-2">{FormData.title}</span>
           </div>
-          <div className='flex flex-row justify-around mt-4'>
-          {
-          Candidates.map((item,index)=>{
-            return <Card key={index} walletConnected={walletConnected} 
-            web3ModalRef={web3ModalRef} 
-            Name={item.Name}
-            role={item.role}/>
-          })
-          }
+          <div className="flex flex-row justify-around mt-4">
+            {Candidates.map((item, index) => {
+              return (
+                <Card
+                  key={index}
+                  walletConnected={walletConnected}
+                  web3ModalRef={web3ModalRef}
+                  Name={item.Name}
+                  role={item.role}
+                />
+              );
+            })}
+          </div>
         </div>
-        </div>
+      </div>
     </div>
   );
 }
 
-
-
-export default dashboard
+export default dashboard;
