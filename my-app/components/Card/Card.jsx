48bdf9e7e34b5201824b5a1c918e9c04ca9653ca
Somesh-Debnath <debnathsomesh1@gmail.com>
2023-05-19 10:33:10 +0530

    fix bug

--- my-app/components/Card/Card.jsx
+++ my-app/components/Card/Card.jsx
@@ -1,21 +1,51 @@
 import { useEffect, useRef, useState } from "react";
-import { doc, setDoc, updateDoc, arrayRemove,arrayUnion,getDocs,collection,query } from "@firebase/firestore";
+import { doc, setDoc, updateDoc, arrayRemove,arrayUnion,getDocs } from "@firebase/firestore";
 import { db} from "../../utils/Firebase";
-import { useCollectionData } from "react-firebase-hooks/firestore";
 import Web3Modal  from 'web3modal'
+import useAuth from "../../hooks/useAuth";
 import { useRouter } from 'next/router';
 
-function Card({walletConnected,people,Name,role,voted,parentCallback,indx,id,eid,Email,Image}) {
-
- const [user, setUsers] = useState(null);
+function Card({state,walletConnected,people,Name,role,parentCallback,voted,indx,id,eid,Email,Image}) {
+  
+//const [voted, setVoted] = useState(0);
+ const [loading, setLoading] = useState(false);
+ const {user,loading:userloading}=useAuth();
  const [owner, setOwner] = useState('');
- const userQuery=collection(db,'users');
-  const [docs,loading,error]=useCollectionData(userQuery);
  console.log(indx, Name)
+ console.log(user?.email , user?.uid)
  const web3ModalRef = useRef();
 
+ //const isVoted=votes.includes(user?.id)
+ //const {toggleVote,isLoading}=useToggleVote({id,isVoted,uid:user?.id});
+// const [walletConnected, setWalletConnected] = useState(false);
+
+// useEffect(() => {
+//   const owner=constants.methods.admin().call();
+//   web3.eth.requestAccounts().then(console.log);
+//   setOwner(owner);
+// }, []);
 console.log(owner);
  const router = useRouter();
+//console.log(FormData.name)
+
+const getProviderOrSigner = async (needSigner = false) => {
+  // Connect to Metamask
+  // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
+  const provider = await web3ModalRef.current.connect();
+  const web3Provider = new providers.Web3Provider(provider);
+  console.log("web3Provider", web3Provider);
+
+  // If user is not connected to the Goerli network, let them know and throw an error
+  const { chainId } = await web3Provider.getNetwork();
+  if (chainId !== 5) {
+    window.alert("Change the network to Goerli");
+    throw new Error("Change network to Goerli");
+  }
+    const signer = web3Provider.getSigner();
+    return signer;
+  
+  return web3Provider;
+};
 
 web3ModalRef.current = new Web3Modal({
   network: "goerli",
@@ -23,28 +53,30 @@ web3ModalRef.current = new Web3Modal({
   disableInjectedProvider: false,
 });
 
-    useEffect(()=>{
-   
-      const getUsers=async()=>{
-        const getUsers=await getDocs(userQuery);
-        const users = getUsers.docs.map((doc)=>({
-          ...doc.data(), id:doc.id
-      }))
-        setUsers(users);        
-      }
-      getUsers();
-    },[])
 
-    console.log(user)
+    // const giveVote = async () => {
+    //   setLoading(true);
+    //   try {
+    //     const signer = await getProviderOrSigner();
+    //     const contract = new Contract(VOTE_CONTRACT_ADDRESS, abi, signer);
+    //     const tx = await contract.vote(0);
+    //     const tx2=await contract.candidatesDetail;
+    //     console.log(tx2);
+    //     await tx.wait();
+    //     setVoted(true);
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    //   setLoading(false);
+    // };
 
+console.log(walletConnected);
   const renderButton = () => {
     if(walletConnected){
         return <button className={`bg-[#93278F] text-white px-8 py-2
         hover:bg-[#5c0f59] ${voted ? "opacity-50 cursor-not-allowed" : ""}
         text-sm rounded-2xl`}
-        onClick={vot} disabled={voted ? true : false}>
-          Vote
-        </button>   
+        onClick={vot}>Vote</button>   
     }
   }
  const vot= async()=>{
@@ -64,13 +96,14 @@ web3ModalRef.current = new Web3Modal({
   
   const docRef = doc(db, "Elections", eid, "Candidates", indx); 
          updateDoc(docRef, {            
-            count: voted ? arrayRemove(user?.uid) : arrayUnion(user?.uid)     
+            count: voted ? arrayRemove(user?.uid) : arrayUnion(user?.uid),     
         });
     alert("Successfully Voted for "+Name);
     parentCallback(1);
 
     console.log(docRef);
-}
+
+  }
  
   return (
     <div className='flex flex-col shadow-lg max-h-[420px]'>
@@ -90,7 +123,7 @@ web3ModalRef.current = new Web3Modal({
               </div>
             </div>
         </div>            
-    
+        
   )
 }
 
